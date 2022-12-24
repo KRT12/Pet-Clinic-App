bda9e6521caeb98c27daf62017058bf79fe61112
Kishan R Thawani <krthawani@gmail.com>
2022-12-24 16:49:16 +0530

    implemented PetTypeService

---
+++ src/main/java/com/example/sfgpetclinic/services/map/PetTypeService.java
@@ -0,0 +1,36 @@
+package com.example.sfgpetclinic.services.map;
+
+import com.example.sfgpetclinic.models.Pet;
+import com.example.sfgpetclinic.models.PetType;
+import com.example.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeService extends AbstractMapService<PetType,Long> implements CrudService<PetType, Long> {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
