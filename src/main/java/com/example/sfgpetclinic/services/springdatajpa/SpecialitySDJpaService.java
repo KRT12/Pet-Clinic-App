b0aea1e415a534f8fbb298f14b4f48c01d262e43
Kishan R Thawani <krthawani@gmail.com>
2023-01-14 11:15:38 +0530

    implemented VisitSDJpaService

--- src/main/java/com/example/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
+++ src/main/java/com/example/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -3,10 +3,14 @@ package com.example.sfgpetclinic.services.springdatajpa;
 import com.example.sfgpetclinic.models.Speciality;
 import com.example.sfgpetclinic.repositories.SpecialityRepository;
 import com.example.sfgpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class SpecialitySDJpaService implements SpecialityService {
 
     private final SpecialityRepository specialityRepository;
